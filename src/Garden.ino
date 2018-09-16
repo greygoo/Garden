52ebf7a2a540299ce1a329a9bd572a9a5eff5ce9
Your Name <you@example.com>
2018-09-16 23:58:45 +0200

    Add simple test to control code and improve data handling

--- src/Garden.ino
+++ src/Garden.ino
@@ -20,5 +20,5 @@ void setup() {
 
 void loop() {
   handleSerial();
-  delay(1000);
+//  delay(1000);
 }
--- src/python/Garden.py
+++ src/python/Garden.py
@@ -6,6 +6,8 @@ import sys
 
 baudrate = 9600
 
+log = open('/tmp/sensorlog', 'a')
+
 ser = serial.Serial()
 ser.port = sys.argv[1]
 ser.baudrate = baudrate
@@ -20,60 +22,122 @@ while ser.in_waiting > 0:
 
 def getData():
     ser.write(b'cD\r\n')
-    time.sleep(0.2)
+    time.sleep(0.5)
     while ser.in_waiting > 0:
-        serial_line = ser.readline().split(",");
+        serial_line = ser.readline().rstrip('\r\n').split(",");
+        print serial_line
         if serial_line[0] == 'data':
+            serial_line.pop(0)
             return serial_line
         
 
 def lightOn():
     ser.write(b'cL\r\n')
-    time.sleep(0.2)
+    time.sleep(1)
     while ser.in_waiting > 0:
-        print ser.readline()
+        print ser.readline().rstrip('\r\n')
 
 
 def lightOff():
     ser.write(b'cl\r\n')
-    time.sleep(0.2)
+    time.sleep(1)
     while ser.in_waiting > 0:
-        print ser.readline()
+        print ser.readline().rstrip('\r\n')
 
 
 def pumpOn(pump):
-    ser.write(b'cP%d' % pump)
-    time.sleep(0.2)
+    ser.write(b'cP%d\r\n' % pump)
+    time.sleep(1)
     while ser.in_waiting > 0:
-        print ser.readline()
+        print ser.readline().rstrip('\r\n')
 
 
 def pumpOff(pump):
-    ser.write(b'cp%d' % pump)
-    time.sleep(0.2)
+    ser.write(b'cp%d\r\n' % pump)
+    time.sleep(1)
     while ser.in_waiting > 0:
-        print ser.readline()
+        print ser.readline().rstrip('\r\n')
 
 
 def fanOn(fan):
-    ser.write(b'cF%d' % fan)
-    time.sleep(0.2)
+    ser.write(b'cF%d\r\n' % fan)
+    time.sleep(1)
     while ser.in_waiting > 0:
-        print ser.readline()
+        print ser.readline().rstrip('\r\n')
 
 
 def fanOff(fan):
-    ser.write(b'cf%d' % fan)
-    time.sleep(0.2)
+    ser.write(b'cf%d\r\n' % fan)
+    time.sleep(1)
     while ser.in_waiting > 0:
-        print ser.readline()
+        print ser.readline().rstrip('\r\n')
 
 
 def fanSpeed(speed):
-    ser.write(b'cs%d' % speed)
-    time.sleep(0.2)
+    ser.write(b'cs%d\r\n' % speed)
+    time.sleep(1)
     while ser.in_waiting > 0:
-        print ser.readline()
+        print ser.readline().rstrip('\r\n')
 
 
-print getData();
+def writeSensorData():
+    sensor_data = getData()
+    print('Temperature Air  : %f' % float(sensor_data[0]))
+    print('Temperature Water: %f' % float(sensor_data[2]))
+    print('Humidity         : %f' % float(sensor_data[1]))
+    for data in sensor_data:
+        log.write(data)
+        log.write(',')
+    log.write("\n")
+
+
+def testFan():
+    fanOn(0)
+    time.sleep(3)
+
+    fanSpeed(1)
+    time.sleep(3)
+    fanSpeed(2)
+    time.sleep(3)
+    fanSpeed(3)
+    time.sleep(3)
+    fanSpeed(4)
+    time.sleep(3)
+    fanSpeed(5)
+    time.sleep(3)
+    fanSpeed(6)
+    time.sleep(3)
+    fanSpeed(7)
+    time.sleep(3)
+    fanSpeed(8)
+    time.sleep(3)
+    fanSpeed(9)
+    time.sleep(3)
+
+    fanOff()
+    time.sleep(3)
+
+
+def testLight():
+    lightOn()
+    time.sleep(3)
+    lightOff()
+    time.sleep(3)
+
+
+def testPump():
+    pumpOn(0)
+    time.sleep(2)
+    pumpOff(0)
+    time.sleep(2)
+
+    pumpOn(1)
+    time.sleep(2)
+    pumpOff(1)
+    time.sleep(2)
+
+
+testLight()
+
+while True:
+    writeSensorData()
