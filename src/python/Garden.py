80e59d6fa9f849cf3043fd4bc76757f59734bc2f
greygoo <chuller@linux-it4g.suse>
2018-09-16 17:29:58 +0200

    improve python frontend

--- src/python/Garden.py
+++ src/python/Garden.py
@@ -6,7 +6,7 @@ import time
 ser = serial.Serial('/dev/ttyUSB1', 9600)
 print 'Serial connection initialised'
 
-while 1:
+def get_data():
     ser.write(b'cD\r\n')
     time.sleep( 1 )
     print ser.readline();
