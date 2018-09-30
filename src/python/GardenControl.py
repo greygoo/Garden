120230073f0ffdcddea23dc4ba2c57a07506c4e8
greygoo <chuller@linux-it4g.suse>
2018-09-30 16:59:53 +0200

    Add function calls for actual controlling the hw

--- src/python/GardenControl.py
+++ src/python/GardenControl.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
 
+import serial
+import time
+import sys
+from GardenLib import *
 
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+
+baudrate = 9600
+
+ser = serial.Serial()
+ser.port = sys.argv[1]
+ser.baudrate = baudrate
+ser.open()
+time.sleep(10)
+print 'Serial connection established'
+
 
 class MyWindow(Gtk.Window):
 
@@ -13,11 +27,11 @@ class MyWindow(Gtk.Window):
         grid = Gtk.Grid()
         self.add(grid)
 
-        button_fanon = Gtk.Button(label="Fan ON")
-        button_fanon.connect("clicked", self.on_button_fanon_clicked)
+        button_fan0on = Gtk.Button(label="Fan ON")
+        button_fan0on.connect("clicked", self.on_button_fan0on_clicked)
 
-        button_fanoff = Gtk.Button(label="Fan OFF")
-        button_fanoff.connect("clicked", self.on_button_fanoff_clicked)
+        button_fan0off = Gtk.Button(label="Fan OFF")
+        button_fan0off.connect("clicked", self.on_button_fan0off_clicked)
 
         button_pump0on = Gtk.Button(label="Pump0 ON")
         button_pump0on.connect("clicked", self.on_button_pump0on_clicked)
@@ -37,38 +51,46 @@ class MyWindow(Gtk.Window):
         button_lightoff = Gtk.Button(label="Light OFF")
         button_lightoff.connect("clicked", self.on_button_lightoff_clicked)
 
-        grid.add(button_fanon)
-        grid.attach_next_to(button_fanoff, button_fanon, Gtk.PositionType.BOTTOM, 1, 1)
-        grid.attach_next_to(button_pump0on, button_fanon, Gtk.PositionType.RIGHT, 1, 1)
+        grid.add(button_fan0on)
+        grid.attach_next_to(button_fan0off, button_fan0on, Gtk.PositionType.BOTTOM, 1, 1)
+        grid.attach_next_to(button_pump0on, button_fan0on, Gtk.PositionType.RIGHT, 1, 1)
         grid.attach_next_to(button_pump0off, button_pump0on, Gtk.PositionType.BOTTOM, 1, 1)
         grid.attach_next_to(button_pump1on, button_pump0on, Gtk.PositionType.RIGHT, 1, 1)
         grid.attach_next_to(button_pump1off, button_pump1on, Gtk.PositionType.BOTTOM, 1, 1)
         grid.attach_next_to(button_lighton, button_pump1on, Gtk.PositionType.RIGHT, 1, 1)
         grid.attach_next_to(button_lightoff, button_lighton, Gtk.PositionType.BOTTOM, 1, 1)
 
-    def on_button_fanon_clicked(self, widget):
-        print("Turn on fan")
+    def on_button_fan0on_clicked(self, widget):
+        print("Turn on fan0")
+        fanOn(ser, 0)
 
-    def on_button_fanoff_clicked(self, widget):
-        print("Turn off fan")
+    def on_button_fan0off_clicked(self, widget):
+        print("Turn off fan 0")
+        fanOff(ser, 0)
 
     def on_button_pump0on_clicked(self, widget):
         print("Turn on pump0")
+        pumpOn(ser,0)
 
     def on_button_pump0off_clicked(self, widget):
         print("Turn on pump1")
+        pumpOff(ser,0)
 
     def on_button_pump1on_clicked(self, widget):
         print("Turn on pump1on")
+        pumpOn(ser, 1)
 
     def on_button_pump1off_clicked(self, widget):
         print("Turn on pump1off")
+        pumpOff(ser,1)
 
     def on_button_lighton_clicked(self, widget):
         print("Turn on lighton")
+        lightOn(ser)
 
     def on_button_lightoff_clicked(self, widget):
         print("Turn on lightoff")
+        lightOff(ser)
 
 
 win = MyWindow()
