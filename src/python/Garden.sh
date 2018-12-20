e43d317b300aed0618a260dbd1a7f007de659e4a
Your Name <you@example.com>
2018-12-20 14:14:51 +0100

    Adjust startup shell script

--- src/python/Garden.sh
+++ src/python/Garden.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/home/pi/src/Garden/src/python/Garden-work.py /dev/ttyUSB0 2>&1 >> /home/pi/Garden.log
+/home/pi/src/Garden/src/python/Garden.py /dev/ttyUSB0 2>&1 >> /home/pi/Garden.log
