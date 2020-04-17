abf724d6a67eb458d8c5289e08313f2c7453642b
Christian Hueller <chuller@suse.com>
2020-04-17 02:07:37 +0200

    Add scheduling to GardenControl

--- src/ruby/Garden.rb
+++ src/ruby/Garden.rb
@@ -1,18 +1,27 @@
 #!/usr/bin/ruby
 
 require 'serialport'
+require_relative 'Scheduler'
 
 class GARDEN
 
   def initialize()
+    # init some vars
+    $status = "starting up"
+
     # init serial connection
     puts "Connecting to serial port"
     @serial_port = SerialPort.new "/dev/ttyUSB0", 115200 
     @serial_port.read_timeout = 5000
 
+    puts "Creating scheduler"
+    @jobs = SCHEDULER.new
+
+
     # function reading serial port in a loop
     def check_serial(callback)
       #begin
+	puts "Start serial read thread"
         Thread.new do
           while true
             if (input = @serial_port.gets)
@@ -25,6 +34,7 @@ class GARDEN
       #rescue
       #end
     end
+
 
 
     def serial_callback(input)
@@ -44,34 +54,40 @@ class GARDEN
   def send(command)
     case command
     when "light0on"
-      puts "Turning on Light 0"
+      puts "Turning on Light 0 and pause auto mode"
+      @jobs.stop('light')
       @serial_port.puts "cL\n"
     when "light0off"
-      puts "Turning off Light 0"
-      @serial_port.write "cl\n"
+      puts "Turning off Light 0 and pause auto mode"
+      @jobs.stop('light')
+      @serial_port.puts "cl\n"
     when "light0auto"
       puts "Switching Light 0 to automatic mode"
-      # turn on automatic
+      @jobs.start('light')
 
     when "pump0on"
       puts "Turning on Pump 0"
+      @jobs.stop('pump0')
       @serial_port.write "cP0\n"
     when "pump0off"
       puts "Turning off Pump 0"
+      @jobs.stop('pump0')
       @serial_port.write "cp0\n"
     when "pump0auto"
       puts "Switching Pump 0 to automatic mode"
-      # turn on automatic
+      @jobs.start('pump0')
 
     when "pump1on"
       puts "Turning on Pump 1"
+      @jobs.stop('pump1')
       @serial_port.write "cP1\n"
     when "pump1off"
       puts "Turning off Pump 1"
+      @jobs.stop('pump1')
       @serial_port.write "cp1\n"
     when "pump1auto"
       puts "Switching Pump 1 to automatic mode"
-      # turn on automatic
+      @jobs.start('pump1')
 
     when "fan0on"
       puts "Turning on Fan 0"
