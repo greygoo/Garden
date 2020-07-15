b04743b4c43db77d630b76672f2ae27a821edd33
Christian Hueller <chuller@suse.com>
2020-07-16 00:37:59 +0200

    Create config settings for box 2.0 controller

--- src/light.cpp
+++ src/light.cpp
@@ -1,11 +1,9 @@
 #include "light.h"
 
-
 RCSwitch powerSwitch = RCSwitch();
 
-
 void lightInit() {
-  powerSwitch.enableTransmit(10);
+  powerSwitch.enableTransmit(RF_PIN);
   powerSwitch.setProtocol(1);
 }
 
@@ -13,7 +11,6 @@ void lightOff(){
   powerSwitch.sendTriState("00000F000FF0");
   D_PRINTLN("D: Switching light off.");
 }
-
 
 void lightOn(){
   powerSwitch.sendTriState("00000F000FFF");
