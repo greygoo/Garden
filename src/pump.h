c959540d833c50586f0d6f2f06259cffba331b24
Christian Hueller <chuller@suse.com>
2020-07-27 18:07:18 +0200

    add ShiftRegister74HC595 submodule and use it to control pumps connected to a relasi behind the switching register.

--- src/pump.h
+++ src/pump.h
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "define.h"
+#include <ShiftRegister74HC595.h>
 
-void pumpInit();
+void pumpInit(ShiftRegister74HC595<1> sr);
 
-void ctrlWaterPump(int pump, bool state);
+void ctrlWaterPump(int pump, bool state, ShiftRegister74HC595<1> sr);
--- src/test.cpp
+++
@@ -1,8 +0,0 @@
-#include "test.h"
-
-void pumpTest(int pump){
-  ctrlWaterPump(pump, 1);
-  delay(200);
-  ctrlWaterPump(pump, 0);
-}
-
