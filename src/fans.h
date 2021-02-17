6ade0df7d2adf283d31215f18d2b8d2bac5c175a
greygoo <chuller@suse.de>
2021-02-17 23:50:07 +0000

    Fix fan speed control

--- src/fans.h
+++ src/fans.h
@@ -1,14 +1,15 @@
 #include "define.h"
-#include <TimerOne.h>
 #include <Arduino.h>
 #include "common.h"
+#include <TimerOne.h>
 
 class Fan
 {
   public:
     Fan();
-    void fanInit();
     void setFan(int fan, bool state);
     void setFanSpeed(int fan_pwm_pin, float dutyCycle);
   private:
+    bool initialized;
+    //TimerOne TimerO;
 };
